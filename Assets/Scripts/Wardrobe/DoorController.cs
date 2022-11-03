ad4324043576c27097a339e76e5e0ea31e451048
evgen <evgeniikuzin@outlook.com>
2022-11-03 23:54:55 +0000

    TV and wardrobe added

---
+++ Assets/Scripts/Wardrobe/DoorController.cs
@@ -0,0 +1,52 @@
+using Assets.Scripts.Protagonist;
+using Assets.Scripts.Wardrobe;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.UI.Image;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] float openSpeed = 10f;
+    [SerializeField] GameObject hinge;
+    [SerializeField] DoorPosition door = DoorPosition.Left;
+    Vector3 originPosition;
+    Quaternion originRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hinge == null)
+        {
+            Debug.Log("hinge is missing");
+        }
+        originPosition = this.transform.position;
+        originRotation = this.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"{transform.rotation}");
+
+        if (door.Equals(DoorPosition.Left))
+        {
+            if (Input.GetKey(KeyCode.L) && this.gameObject.transform.rotation.y < 0.65)
+            {
+                this.transform.RotateAround(hinge.transform.position, Vector3.up, openSpeed * Time.deltaTime);
+            }
+        }
+        if (door.Equals(DoorPosition.Right))
+        {
+            if (Input.GetKey(KeyCode.R) && this.gameObject.transform.rotation.y > 0.65)
+            {
+                this.transform.RotateAround(hinge.transform.position, Vector3.up, -openSpeed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            this.transform.position = originPosition;
+            this.transform.rotation = originRotation;
+        }
+    }
+}
