387666caa41ebfe12bda1ea35dbfa5c9383b497d
evgen <evgeniikuzin@outlook.com>
2022-11-12 13:43:24 +0000

    Wardrobe animation for menu added

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -7,8 +7,10 @@ namespace Assets.Scripts.Game
 {
     public class GameManager : Singleton<GameManager>
     {
-        [SerializeField] Material motivationMessage;
-        public GameState State { get; set; }
+        [SerializeField] Material motivationMessage; // Testing. should come from motivator
+        [SerializeField] GameObject protagonist;
+        [SerializeField] GameObject dummy;
+        public GameState State { get; set; } // TODO private set and refactor
         // Use this for initialization
         void Start()
         {
@@ -18,18 +20,25 @@ namespace Assets.Scripts.Game
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.M)) // sound from the room -> chatter -> creates motivation
+            if (Input.GetKeyDown(KeyCode.M)) // Testing. should come from motivator
             {
-                ProtagonistUIController.Instance.AddToChat(motivationMessage, PositionState.Left); 
+                ProtagonistUIController.Instance.AddToChat(motivationMessage, PositionState.Left);
                 State = GameState.Motivation;
             }
-            if (Input.GetKeyDown(KeyCode.X))
+            if (State.Equals(GameState.Menu))
             {
-                State = GameState.End;
+                protagonist.SetActive(false);
+                dummy.SetActive(true);
+            }
+            if (!State.Equals(GameState.Menu))
+            {
+                protagonist.SetActive(true);
+                dummy.SetActive(false);
             }
             if (State.Equals(GameState.Menu) || State.Equals(GameState.Motivation))
             {
                 Camera.main.GetComponent<CinemachineBrain>().enabled = false;
+
             }
             else if (!Camera.main.GetComponent<CinemachineBrain>().enabled)
             {
---
+++ Assets/Scripts/Misc/WardrobeAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Misc
+{
+    public class WardrobeAnimation : StateMachineBehaviour
+    {
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.Play("Haunted_Wardrobe_Menu", -1, Random.Range(0f, 1f));
+        }
+    }
+}
