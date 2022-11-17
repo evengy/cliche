f3426b3dccdead581c7f739dce9d265c07c2dcf8
evgen <evgeniikuzin@outlook.com>
2022-11-17 00:10:06 +0000

    Menu Scene Animations completed

---
+++ Assets/Scripts/Game/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Helpers;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class MenuManager : Singleton<MenuManager>
+    {
+        [SerializeField] Animator chairAnimator;
+        [SerializeField] Animator wardrobeAnimator;
+        [SerializeField] Animator handAnimator;
+        [SerializeField] Animator bookAnimator;
+
+        float timer;
+        private void Start()
+        {
+            timer = 0;
+        }
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (!GameManager.Instance.State.Equals(GameState.Menu))
+            {
+                chairAnimator.enabled = false;
+            }
+            else
+            {
+                if (timer > 10f)
+                {
+                    timer = 0;
+                    chairAnimator.SetTrigger("GetBook");
+                    bookAnimator.SetTrigger("Appear");
+                    wardrobeAnimator.SetTrigger("Open");
+                    handAnimator.SetTrigger("Take");
+                }
+            }
+        }
+    }
+}
