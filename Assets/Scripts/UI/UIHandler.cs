615c9f1770026d3b67c9dd9924d9f48c7e1d93eb
evgen <evgeniikuzin@outlook.com>
2022-11-17 21:13:18 +0000

    menu buttons transition animations added

---
+++ Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.UI
+{
+    public class UIHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] AudioClip buttonSelected;
+        AudioSource source;
+        [SerializeField] float volume = 0.5f;
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!source.isPlaying)
+                source.Play();
+            Debug.Log("hover over button");
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            //source.Stop();
+            Debug.Log("hover is over");
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            source = gameObject.AddComponent<AudioSource>();
+            source.loop = false;
+            source.clip = buttonSelected;
+            source.volume = volume;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
